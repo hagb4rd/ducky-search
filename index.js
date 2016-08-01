bfb05061b4db3c75f768551b47bdc42ba90c9cc8
hagb4rd <hagb4rd@gmail.com>
2016-08-01 21:07:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-var ducky = module.exports = function ducky(query, site) { if (site) { site = "+" + encodeURIComponent("site:" + site); } else { site = ""; }; var url = ("https://duckduckgo.com/?q=!ducky+%q" + site).replace("%q", encodeURIComponent( query.replace(" ", "+") ) ); return url; }
+var ducky = function ducky(query, site) { if (site) { site = "+" + encodeURIComponent("site:" + site); } else { site = ""; }; var url = ("https://duckduckgo.com/?q=!ducky+%q" + site).replace("%q", encodeURIComponent( query.replace(" ", "+") ) ); return url; }
+
+if(typeof module !== 'undefined') {
+  if(module.exports)
+    module.exports = ducky;
+}
